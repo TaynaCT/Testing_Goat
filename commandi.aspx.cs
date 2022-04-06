4f158c3a0e500998e64db297dcb6f38574582f81
Tayná Tinôco <tayna-ct12@hotmail.com>
2022-04-06 16:24:37 +0100

    Add files via upload
    The original files are from  https://github.com/CxCE/Exercises

---
+++ commandi.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+//Trying to fing the issue here !!! 
+
+
+namespace CxCE_Demo
+{
+    public partial class commandi : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void submit_Click(object sender, EventArgs e)
+        {
+            string processtostart = systemname.Text;
+            
+            Process.Start(processtostart);            
+        }
+    }
+}
